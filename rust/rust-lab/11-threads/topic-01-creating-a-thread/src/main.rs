5600ad341b55c1e056c090d56348962623d04825
dezlymacauley <dezlymacauley@proton.me>
2025-09-11 09:10:43 +0200

    Updated my knowledge of Threads in Rust

--- rust/rust-lab/11-threads/topic-01-creating-a-thread/src/main.rs
+++ rust/rust-lab/11-threads/topic-01-creating-a-thread/src/main.rs
@@ -2,29 +2,107 @@
 
 ABOUT: Thread
 
-A thread is a block of code that runs separately from the main thread
-(aka fn main).
+Here's a good anology:
 
-The easiest way to think of a thread,
-is to view it as a side program that runs alongside the main thread,
-without blocking its flow.
+_______________________________________________________________________________c
+
+1. A default Rust program:
+
+Think of this as a company that starts
+off with only one office, and one office manager.
+The office is called `fn main`, and the manager is `The Rust Compiler`.
+
+You can think of the `The Rust Compiler` as the most import manager
+of the company. Aka the CEO of the company.
+
+The Rust Compiler is responsible for the `main` progress of the company.
+
+_______________________________________________________________________________c
+
+2. A Thread:
+
+Think of a thread as an office that your program is given
+to complete all the tasks that you have told it to perform.
+
+_______________________________________________________________________________c
+
+3. A Join Handle:
+
+Think of this as the office manager for a specific office.
+This manager opens the office, ensures that work is done,
+and then reports back to the CEO (aka the Rust compiler) when work is doen.
+
+_______________________________________________________________________________c
+
+Just like how a company can have mutiple offices and mutiple managers,
+it is also possible for a Rust program to have multiple threads and mutiple
+join handles.
+
+_______________________________________________________________________________c
 
 */
 
-// This will bring the `thread` functionality from the Rust standard library
-// into scope.
+// This line tells the Rust compiler that I intend to use the `thread` module
+// from the Rust standard library.
+//
+// This is a module that contains many useful functions
+// for creating your own threads and managing those threads.
 use std::thread;
 
+// This is the variable type of the join handle that will be used to manage
+// threads.
+use std::thread::JoinHandle;
+
+// When run your compiled Rust program,
+// the operating system gives your program one thread by default.
+//
+// The default thread for a Rust program is `fn main()`.
 fn main() {
+    // This will be printed by the main thread.
     println!("Message A");
+
+    // When you want to create a thread in Rust,
+    // the first thing that you have to do is to create a `join handle` aka
+    // an office manager.
 
     // `thread::spawn` creates and starts a new thread.
     // The spawn function returns a join handle.
-    thread::spawn(|| {
+
+    // The variable type is JoinHandle<T>
+    // T is the variable type that is returned by the closure.
+    //
+    // Currently I am not returning anything back from the closure,
+    // I am simply printing to the terminal so T is ().
+    // () is called the unit type. It is used to show that a function
+    // does not return anything.
+    let join_handle: JoinHandle<()> = thread::spawn(|| {
         println!("Message B");
     });
 
+    // After "Message A" is printed. The program will skip ahead to the next
+    // instruction and print "Message C". This is because "Message B" is
+    // being processed in a different thread outside the flow of `fn main()`
     println!("Message C");
 
-    // Message B will not be printed
+    // To continue the anology.
+    // This is the office manager in action.
+    // `.join()` tells the main thread `fn main()`, to not proceed with
+    // the program until it gets the work that is being processed
+    // by the office manager's office.
+
+    // In simple terms, `fn main()` is told:
+    // Don't print `Message D`, until you have received message "Message B"
+    // from me. So `.join()` is a blocking method.
+
+    // `.unwrap()` tells the program to crash if immediately if it fails to
+    // get a response ("Message B")
+    join_handle.join().unwrap();
+
+    println!("Message D");
 }
+
+// This is what will be printed.
+// Message A
+// Message C
+// Message B
+// Message D
