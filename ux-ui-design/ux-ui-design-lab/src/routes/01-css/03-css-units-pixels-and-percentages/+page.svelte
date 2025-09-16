3b6750e6ca2d9d1895df62f83ef56b98d6214bb0
dezlymacauley <dezlymacauley@proton.me>
2025-09-16 15:50:27 +0200

    More effore on CSS

---
+++ ux-ui-design/ux-ui-design-lab/src/routes/01-css/03-css-units-pixels-and-percentages/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import "./page.css";
+</script>
+
+<!---------------------------------------------------------------------------->
+
+<!-- 
+
+The two dive tags below are inside the `<body></body>` HTML tag.
+
+The tag is not shown here because in Svelte,
+everything inside a Svelte file (excluding the script and style tags),
+is is inside the <body></body> tag.
+
+-->
+<div class="entire-body">The full width of the `body` html tag</div>
+<div class="half-of-body">50 Percent of `body` container</div>
+<!---------------------------------------------------------------------------->
+
+<div class="container-one">
+  <!-- These will take 25% of its parent, which is container-one  -->
+  <div class="item">Item 1</div>
+  <div class="item">Item 2</div>
+  <div class="item">Item 3</div>
+</div>
--- ux-ui-design/ux-ui-design-lab/src/routes/01-css/03-css-units/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import "./page.css";
-</script>
-
-<div class="item item-percent">50 Percent</div>
-<div class="parent">
-  <div class="item"></div>
-  <div class="item"></div>
-</div>
