efe3e9c4a31c50b35bf475f130c0edbb3f28edbd
dezlymacauley <dezlymacauley@proton.me>
2025-09-18 11:09:01 +0200

    More Rust training

--- rust/rust-lab/07-enums-and-error-handling/topic-07-enums-with-struct-variants/src/main.rs
+++ rust/rust-lab/07-enums-and-error-handling/topic-07-enums-with-struct-variants/src/main.rs
@@ -1,3 +1,59 @@
+enum AttackDamage {
+    SwordAttack { damage: i32 },
+    MagicAttack { damage: i32, mana_cost: i32 },
+    GunAttack { damage: i32, ammo_cost: i32 },
+}
+
+use AttackDamage::*;
+
 fn main() {
-    println!("Hello, world!");
+    let attack_one = SwordAttack { damage: 30 };
+    let attack_two = MagicAttack { damage: 600, mana_cost: 50 };
+    let attack_three = GunAttack { damage: 150, ammo_cost: 1 };
+
+    match attack_one {
+        SwordAttack { damage } => println!("Sword attack: {}", damage),
+
+        MagicAttack { damage, mana_cost } => {
+            println!("Magic attack: {}", damage);
+            println!("Mana cost: {}", mana_cost);
+        },
+        GunAttack { damage, ammo_cost } => {
+            println!("Gun attack: {}", damage);
+            println!("Ammo cost: {}", ammo_cost);
+        },
+    }
+
+    match attack_two {
+        SwordAttack { damage } => println!("Sword attack: {}", damage),
+
+        MagicAttack { damage, mana_cost } => {
+            println!("Magic attack: {}", damage);
+            println!("Mana cost: {}", mana_cost);
+        },
+        GunAttack { damage, ammo_cost } => {
+            println!();
+            println!("Gun attack: {}", damage);
+            println!("Ammo cost: {}", ammo_cost);
+        },
+    }
+
+    match attack_three {
+        SwordAttack { damage } => println!("Sword attack: {}", damage),
+
+        MagicAttack { damage, mana_cost } => {
+            println!("Magic attack: {}", damage);
+            println!("Mana cost: {}", mana_cost);
+        },
+        GunAttack { damage, ammo_cost } => {
+            println!("Gun attack: {}", damage);
+            println!("Ammo cost: {}", ammo_cost);
+        },
+    }
+
+    // Sword attack: 30
+    // Magic attack: 600
+    // Mana cost: 50
+    // Gun attack: 150
+    // Ammo cost: 1
 }
