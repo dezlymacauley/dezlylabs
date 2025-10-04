d841bdbdc2091654c483b629221734029d880dfc
dezlymacauley <dezlymacauley@proton.me>
2025-10-04 09:29:42 +0200

    Learned how structs work in Google

--- software-engineering/go/go-lab/07-structs/topic-01-basics-of-structs/main.go
+++ software-engineering/go/go-lab/07-structs/topic-01-basics-of-structs/main.go
@@ -2,19 +2,57 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+// NOTE: Please note that the casing of the first letter matters in Go
+
+// lowercase means the struct is private. I.e. Can only be accessed from
+// this file.
+
+// Uppercase means the struct is public. I.e. It can be accessed outside of
+// this file.
+
+// type NameOfStruct struct {}
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
+
 func getUserData(promptText string) string {
-    var value string = ""
-    fmt.Print(promptText)
-    
+	var value string = ""
+	fmt.Print(promptText)
+
 	fmt.Scan(&value)
 	return value
 }
 
+func outputUserDetails(firstName string, lastName string, birthDate string) {
+	fmt.Println(firstName, lastName, birthDate)
+}
+
 func main() {
-	firstName := getUserData("Please enter your first name: ")
-	lastName := getUserData("Please enter your last name: ")
-	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
-	fmt.Println(firstName, lastName, birthdate)
+	userFirstName := getUserData("Please enter your first name: ")
+	userLastName := getUserData("Please enter your last name: ")
+	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	// NOTE: This is how to create an instance of a struct
+
+    // When you declare an instance of a struct like this, 
+    // the right side of the equals sign is called a `composite literal`,
+    // or to be more specific a `struct literal`
+
+    // The term `composite literal` is a general term in Go.
+    // It refers to expressions like T{...}, where T can be
+    // an array, slice, map, or struct type.
+    var appUser user = user{
+       firstName: userFirstName,
+       lastName: userLastName,
+       birthDate: userBirthdate,
+       createdAt: time.Now(),
+    }
+
+	outputUserDetails(userFirstName, userLastName, userBirthdate)
 }
