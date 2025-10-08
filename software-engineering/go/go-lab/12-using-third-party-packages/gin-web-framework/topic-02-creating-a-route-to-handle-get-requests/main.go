3f3631776725c44eac75436b02637d340d620fdb
dezlymacauley <dezlymacauley@proton.me>
2025-10-09 01:26:54 +0200

    Recap on how the Gin package works

--- software-engineering/go/go-lab/12-using-third-party-packages/gin-web-framework/topic-02-creating-a-route-to-handle-get-requests/main.go
+++ software-engineering/go/go-lab/12-using-third-party-packages/gin-web-framework/topic-02-creating-a-route-to-handle-get-requests/main.go
@@ -1,37 +1,63 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+func getEvents(context *gin.Context) {
+    // An HTTP status code is a three-digit number sent by a web server 
+    // in response to a client’s request (like a browser 
+    // or an API client). 
+    // It tells the client what happened with its request.
+    // 200 means "OK", as in the server was succesfully able to get a
+    // response back from the client.
+
+    // NOTE: 200 means that the server successfully received, understood, 
+    // and processed the client's request. 
+    // It does not guarantee that the response content 
+    // is correct for your application logic.
+
+    // Go has a built in http module so that you don't have to remember
+    // the number of each HTTP status code.
+
+    // This is the same as: 
+    // httpStatusCode = 200
+    httpStatusCode := http.StatusOK
+
+    jsonResponse := map[string]string{
+        // key: value
+        "message": "hello",
+    }
+
+    // `.JSON` converts the `jsonResponse` map into JSON
+    // and sends it back to the client with the given status code.
+    context.JSON(httpStatusCode, jsonResponse)
+}
+
 func main() {
+	server := gin.Default()
 
-    // This creates an Engine struct using the third-party package gin.
-    // This Engine struct is basically an HTTP server that can listen
-    // for requests on a specific address.
-    // The Engine struct also contains some useful features like a `Logger`,
-    // to log requests (useful for debugging and data analytics), 
-    // and a `Recovery` middleware that makes the server resilient against
-    // crashes.
+    // `context` is a pointer to an instance of the Context struct from
+    // the `gin` package.
+    // It contains information about the incoming request (headers, query params, 
+    // body, etc.) from the client,
+    // and it also provides methods (like `.JSON`, `.String`, `.HTML`) 
+    // to send a response back to the client.
 
-    var server *gin.Engine = gin.Default() 
+    // In this example, I am dealing with a simple GET request, so I don't
+    // need to read the data from the client, I just need to send JSON
+    // data back to the client.
+	server.GET("/events", getEvents)
 
-    // This will start the server and listen for connections at:
-    // http://127.0.0.1:8080
+	server.Run("127.0.0.1:8080")
+    /*
 
-    // 127.0.0.1 is the local network on your computer that is not exposed
-    // to the internet. This is often used for testing.
-    server.Run("127.0.0.1:8080") 
+        {
+            "message": "hello"
+        }
 
-    // You should get this message:
-    // 404 page not found
+    */
 
-    // This is because you have to set up a `handler` for that route.
-    // A `handler` is basically an instruction (usually a function) 
-    // for what the server should do when a request is made 
-    // to one of its routes.
-
-    // Since you have not given the server any instructions, 
-    // it is responding with a default plain text response of 
-    // "404 page not found"
 }
--- software-engineering/go/go-lab/14-projects/rest-api/main.go
+++ software-engineering/go/go-lab/14-projects/rest-api/main.go
@@ -11,7 +11,7 @@ import (
 	// SUB_SECTION: Developer-Created modules
 
 	// Contains the structure of an event and event related functions
-	"rest-api/models"
+	// "rest-api/models"
 
 	//_________________________________________________________________________
 	// SUB_SECTION: Third Party modules
@@ -22,36 +22,19 @@ import (
 )
 
 func getEvents(context *gin.Context) {
+    httpStatusCode := http.StatusOK
 
-    events := models.GetAllEvents()
+    jsonResponse := map[string]string{
+        "message": "hello",
+    }
 
-    // This will send back data that is formatted as JSON
-    // This JSON function accepts and HTTP status code,
-    // and it also accepts the data that you want to send back to the client
-
-    // 200 status code means everything worked.
-    // http.StatusOK = 200
-    // This is a map in Go
-    // context.JSON(http.StatusOK, gin.H{"message": "Hello!"})
-
-    // 
-    context.JSON(http.StatusOK, events)
-}
-
-func createEvent(context *gin.Context) {
-
+    context.JSON(httpStatusCode, jsonResponse)
 }
 
 func main() {
-    // This will create an HTTP server
-    server := gin.Default()
+	server := gin.Default()
 
-    // This accepts a url, and the name of a function 
-    // that will handle the GET request
-    server.GET("/events", getEvents)
+	server.GET("/events", getEvents)
 
-    server.POST("/events", createEvent)
-
-    // This will start the sever and start listening for HTTP requests.
-    server.Run(":8080") // localhost:8080
+	server.Run("127.0.0.1:8080")
 }
