8943582ec6718718ce43b4fc761cca89f97ba7b7
dezlymacauley <dezlymacauley@proton.me>
2025-10-24 20:49:07 +0200

    Done for today

--- zig-lab/01-project-setup/topic-02-constants/src/main.zig
+++
@@ -1,4 +0,0 @@
-pub fn main() !void {
-    const age = 70;
-    age = 50;
-}
---
+++ zig-lab/02-project-templates/basic-zig-project/main.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+
+pub fn sumOfTwo(a:i32, b:i32) {
+
+}
+
+pub fn main() void {
+    std.debug.print(comptime fmt: []const u8, args: anytype);
+}
