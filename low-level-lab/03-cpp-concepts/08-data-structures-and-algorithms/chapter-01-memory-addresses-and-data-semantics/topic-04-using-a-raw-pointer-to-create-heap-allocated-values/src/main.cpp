fd8d951e990e1c6b64e1b449a23453ce4cc5890a
dezlymacauley <dezlymacauley@proton.me>
2025-10-31 14:05:41 +0200

    Getting better at C++

--- low-level-lab/03-cpp-concepts/08-data-structures-and-algorithms/chapter-01-memory-addresses-and-data-semantics/topic-04-using-a-raw-pointer-to-create-heap-allocated-values/src/main.cpp
+++ low-level-lab/03-cpp-concepts/08-data-structures-and-algorithms/chapter-01-memory-addresses-and-data-semantics/topic-04-using-a-raw-pointer-to-create-heap-allocated-values/src/main.cpp
@@ -1,36 +1,40 @@
-/* 
+/*
 
-ABOUT: Creating Heap-Allocated Values With A Raw Pointer
+ABOUT: Using a raw pointer to create Heap-allocated values
 
 */
 
 #include <cstdio>
 
 int main() {
-    // pointer_to_a is of the type `int*`
+    // pointer_to_int_value is of the type `int*`
     // `int*` means a pointer an integer value
-    // variable_a is a pointer, which means that it stores a memory address,
+
+    // pointer_to_int_value is a pointer,
+    // which means that it stores a memory address,
     // on the stack, while the value 12 is stored in heap memory.
 
     // In simple terms, the pointer is stored on the stack
-    // but the value it points to is stored on the heap  
+    // but the value it points to is stored on the heap
     // via the `new T()` syntax.
+
     // This will reserve a location in memory that is big enough to store
     // an int variable, and then the value of 12 will be stored there.
-    int* pointer_to_a = new int(12);
-    printf("pointer_to_a stores the memory address: %p\n", pointer_to_a);
-    // pointer_to_a stores the memory address: 0x56456f626320
+    int* pointer_to_int_value = new int(12);
+    printf("pointer_to_int_value stores the memory address: %p\n",
+           pointer_to_int_value);
+    // pointer_to_int_value stores the memory address: 0x55e9fb5ff320
 
     //_________________________________________________________________________
 
-    // To access the value of a pointer you need to 
+    // To access the value of a pointer you need to
     // use this `*`. This is known as the dereference operator.
 
-    printf("pointer_to_a points to the value: %d\n", *pointer_to_a);
-    // pointer_to_a points to the value: 12
+    printf("pointer_to_int_value: %d\n", *pointer_to_int_value);
+    // pointer_to_int_value: 12
 
     //_________________________________________________________________________
-    
+
     // WARNING: Don't forget about cleanup when using raw pointers
 
     // Now the downside to using a raw pointer is that you have to remember
@@ -40,6 +44,6 @@ int main() {
     // longer points to a valid location in memory.
     // This is done to prevent dangling references.
 
-    delete pointer_to_a;
-    pointer_to_a = nullptr;
+    delete pointer_to_int_value;
+    pointer_to_int_value = nullptr;
 }
